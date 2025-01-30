482fc40a204d6a954a7166b825a1f40ca85d1258
unknown <jesudamilola2000@gmail.com>
2025-01-30 22:57:45 +0100

    navbar changed

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  menu: boolean = false;
+  menu_icon:string = 'fa-solid fa-bars fa-2xl';
+
+  openMenu() {
+    this.menu = !this.menu;
+    this.menu_icon = this.menu ? 'fa-solid fa-xmark fa-2xl':'fa-solid fa-bars fa-2xl'
+  }
+
+  closeMenu() {
+    this.menu = false;
+    this.menu_icon = 'fa-solid fa-bars fa-2xl';
+  }
 }
